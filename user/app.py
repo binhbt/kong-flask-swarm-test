66e4c8777ac6223c0d675c9180950e63a250c0ce
BinhBT <binhbt@sigma-solutions.eu>
2020-01-13 11:21:24 +0700

    init

---
+++ user/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import uuid
+import os
+
+app = Flask(__name__)
+randomid = uuid.uuid4()
+@app.route('/')
+def index():
+    hostname = os.uname()[1]
+    return 'Container Hostname: ' + hostname + ' , ' + 'UUID: ' + str(randomid) + '\n'
+
+@app.route('/public')
+def hello_public():
+  return 'User Hello from public!'
+
+@app.route('/user/homepage')
+def hello_homepage():
+  return 'User Hello from homepage! You need logged in to access this page'
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
